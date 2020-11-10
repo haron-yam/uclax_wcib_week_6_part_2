4bc4636893a58451bed9ff2fdf89954f00dc06d3
haron-yam <72232722+haron-yam@users.noreply.github.com>
2020-11-10 15:52:42 -0800

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 console.log('Hello from js/scripts.js!');
 
-
-//const buttonConstand = document.getElementById('buttonUpdate')
-
-
-//create array of all my pictures
-var imgs = ['images/peach.jpg','images/blackberry.jpg','images/grapefruit.jpg','images/watermelon.jpg', 'images/cherrylime.jpg'];
-var colors = ['orange', 'purple', 'red', 'green', 'lime'];
-//set variable marking the location of where I am going to do my picture's function using the element ID
-var imgContainer = document.getElementById('imageContainer');
-
-var clicker = document.getElementById('overlayTest');
-
-var currentImageIdx=0;
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
 
 
-var img = new Image();
-img.src = imgs[currentImageIdx];
-imgContainer.appendChild(img);
-document.getElementById("overlayTest").style.backgroundColor=colors[currentImageIdx];
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel();
 
-clicker.addEventListener('click', function loadPic(){
-//if you copy this section, give Amanda a shoutout
-  currentImageIdx=(currentImageIdx+1)%(imgs.length);
-  var updatedImg = new Image();
-  updatedImg.src=imgs[currentImageIdx];
-  var imgOld=document.getElementById("imageContainer");//.childNodes[0];
-  //console.log(currentImageIdx);
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
 
-  document.getElementById("overlayTest").style.backgroundColor=colors[currentImageIdx];
-  imgContainer.replaceChild(updatedImg, imgOld.childNodes[0]);
-});
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+})
